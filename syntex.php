58dc9f96f35e9835657a06e98405b976c6805626
sazzadshakhbd <sazzadshakhbd@gmail.com>
2019-08-19 09:35:27 +0600

    php syntex

---
+++ syntex.php
@@ -0,0 +1,19 @@
+<doctype html>
+    <html lang="en">
+        <head>
+            <meta charset="utf-8">
+            <title>PHP Syntex</title>
+        </head>
+        <body>
+            <h1>
+            <?php 
+                echo "hello sazzad"
+            ?>
+            </h1>
+            <br>
+            
+            <?php 
+                echo "<p>paragarpah </p>"
+            ?>
+        </body>
+    </html>
