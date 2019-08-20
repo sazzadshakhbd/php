047dfe887be368c55513c7cf8c7be163860c1227
sazzadshakhbd <sazzadshakhbd@gmail.com>
2019-08-20 08:54:29 +0600

    how to decleare datatype

---
+++ data-type.php
@@ -0,0 +1,29 @@
+<doctype html>
+    <html lang="en">
+        <head>
+            <meta charset="utf-8">
+            <title>data-type</title>
+        </head>
+        <body>
+            <?php 
+                $string = "Sazzad";
+                $integer = 329;
+                $float = 3.5;
+                $boolean = false;
+                $null = NULL;
+                $array = array('Tiger','Lion','Snake');
+
+                var_dump($string);
+                echo "<br>";
+                var_dump($integer);
+                echo "<br>";
+                var_dump($float);
+                echo "<br>";
+                var_dump($boolean);
+                echo "<br>";
+                var_dump($null);
+                echo "<br>";
+                var_dump($array);
+            ?>
+        </body>
+    </html>
