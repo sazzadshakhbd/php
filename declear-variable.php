5e6d564128ce84c480b21d52ed49a35dd702cc67
sazzadshakhbd <sazzadshakhbd@gmail.com>
2019-08-20 08:26:28 +0600

    how to decleare variable

---
+++ declear-variable.php
@@ -0,0 +1,21 @@
+<doctype html>
+    <html lang="en">
+        <head>
+            <meta charset="utf-8">
+            <title>Declear Variable</title>
+        </head>
+        <body>
+            <?php 
+                $num = 1;
+                echo "<h1>This Number Is = $num</h1>";
+               
+                $name = "Md Sazzad Ali Shakh";
+                echo "<h2>Your full name is  $name</h2>";
+
+                $num1 = 10;
+                $num2 = 15;
+                $total = $num1 + $num2;
+                echo "<h3>The Total number is =  $total</h3>"
+            ?>
+        </body>
+    </html>
