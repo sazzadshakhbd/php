c24ab07f002fc68fbe67bb675d3cbeb312eecdb8
sazzadshakhbd <sazzadshakhbd@gmail.com>
2019-08-21 08:11:20 +0600

    how to declere constant

---
+++ Smart-Coder/constant.php
@@ -0,0 +1,13 @@
+<doctype html>
+    <html lang="en">
+        <head>
+            <meta charset="utf-8">
+            <title>constant</title>
+        </head>
+        <body>
+            <?php
+               define("name", "Sazzad");
+               echo name; 
+            ?>
+        </body>
+    </html>
