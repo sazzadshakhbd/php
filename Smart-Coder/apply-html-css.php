9f7e47f1493d57eb02b8d6c7741151f511ffbda3
sazzadshakhbd <sazzadshakhbd@gmail.com>
2019-08-21 08:25:10 +0600

    how to apply css and html in php

---
+++ Smart-Coder/apply-html-css.php
@@ -0,0 +1,22 @@
+<doctype html>
+    <html lang="en">
+        <head>
+            <meta charset="utf-8">
+            <title>css-html</title>
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        </head>
+        <body>
+            <?php
+              $html = "
+                        <div class='container'>
+                            <div class='row'>
+                                <div class='col-md-12'>
+                                    <h1 class='text-danger'>Md Sazzad Ali Shakh</h1>
+                                </div>
+                            </div>
+                        </div>
+                    ";
+                    echo $html;
+            ?>
+        </body>
+    </html>
