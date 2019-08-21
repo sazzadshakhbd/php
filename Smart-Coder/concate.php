20482ab21c0becacaa57e701471d46a431d1e2fe
sazzadshakhbd <sazzadshakhbd@gmail.com>
2019-08-21 08:32:55 +0600

    how to add concate

---
+++ Smart-Coder/concate.php
@@ -0,0 +1,18 @@
+<doctype html>
+    <html lang="en">
+        <head>
+            <meta charset="utf-8">
+            <title>css-html</title>
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        </head>
+        <body>
+            <?php
+                $a = "Sazzad";
+                $b = "Shakh";
+                $c = 5.6;
+                echo "<h1 class='text-success'> $a  $b </h1>";
+                echo "<br>";
+                echo "<h1>" . $c . "</h1>"
+            ?>
+        </body>
+    </html>
