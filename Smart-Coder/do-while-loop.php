2e934f00e9314b11a71aada6d8cd1c7aabad28f5
sazzadshakhbd <sazzadshakhbd@gmail.com>
2019-08-25 09:24:24 +0600

    how to apply while do-while loop statement

---
+++ Smart-Coder/do-while-loop.php
@@ -0,0 +1,16 @@
+<doctype html>
+    <html lang="en">
+        <head>
+            <meta charset="utf-8">
+            <title>Do While loop</title>
+        </head>
+        <body>
+            <?php 
+                $x = 11;
+                do {
+                    echo "The Number is ".$x."<br>";
+                    $x++;
+                }while($x <= 10);
+            ?>
+        </body>
+    </html>
