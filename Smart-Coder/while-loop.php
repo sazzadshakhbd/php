789742009f49b1a7a43cbb14de32efda9ed5f1d9
sazzadshakhbd <sazzadshakhbd@gmail.com>
2019-08-25 09:16:04 +0600

    how to apply while loop statement

---
+++ Smart-Coder/while-loop.php
@@ -0,0 +1,16 @@
+<doctype html>
+    <html lang="en">
+        <head>
+            <meta charset="utf-8">
+            <title>while loop</title>
+        </head>
+        <body>
+            <?php 
+                $x = 1;
+                while($x <= 10){
+                    echo "This Number is ". $x . "<br>";
+                    $x++;
+                }
+            ?>
+        </body>
+    </html>
