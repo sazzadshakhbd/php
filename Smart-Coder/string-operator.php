53720f346a7366b32ed2ce5722ef865a7bfb4fa7
Md. Sazzad Ali Shakh <sazzadshakhbd@gmail.com>
2019-08-25 11:35:31 +0600

    how to apply string and logical operator with differnece betwen print and eco

---
+++ Smart-Coder/string-operator.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>String Operator</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+    <?php 
+        $x = "Sazzad";
+        $y = "Shakh";
+
+        echo $x." ".$y;
+
+        echo "<br>";
+
+        $a = "Sobuz";
+        $a.= "Shakh";
+        echo $a;
+    ?>
+</body>
+</html>
