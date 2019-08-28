d5b60f3e28c745cccb7e783cf2a2bc36afc54513
Md. Sazzad Ali Shakh <sazzadshakhbd@gmail.com>
2019-08-28 16:22:25 +0600

    how to apply trim

--- Smart-Coder/trim.php
+++ Smart-Coder/trim.php
@@ -2,19 +2,22 @@
     <html lang="en">
         <head>
             <meta charset="utf-8">
-            <title>array-dimensional</title>
+            <title>trim</title>
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         </head>
         <body>
            <?php 
-           $all = array (
-               array('Tiger', 'Lion'),
-               array('Elephant', 'Snake'),
-               array('Monkey', 'Bear'),
-               array('Cat', 'Dog'),
-               array('Hen')
-           );
-           echo $all[2][1];
+                $a = "Animal";
+                echo trim($a, "A");
+                echo "<br>";
+                var_dump($a);
+                echo "<br>";
+                $b = "Hello Elephant";
+                echo ltrim($b, "Hello");
+                echo "<br>";
+                $c = "Md Sazzad ali shakh";
+                echo rtrim($c, "shakh");
+
            ?>
         </body>
     </html>
