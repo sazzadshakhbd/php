3bc9b1ab35aaf984917ad05ab66af521ee107caf
sazzadshakhbd <sazzadshakhbd@gmail.com>
2019-09-11 00:45:09 +0600

    how to assign assignment operator

---
+++ Udemy/PHP for Beginners A Step-by-Step Guide to Programming PHP/assignment-operator.php
@@ -0,0 +1,29 @@
+<?php 
+  $a = 10;
+  echo $a;
+  echo '<br>';
+
+  $a += 15;
+  echo $a;
+  echo '<br>';
+
+  $a -= 15;
+  echo $a;
+  echo '<br>';
+
+  $a *= 5;
+  echo $a;
+  echo "<br>";
+
+  $a /= 10;
+  echo $a;
+  echo "<br>";
+
+  $a .= 15;
+  echo $a;
+  echo "<br>";
+
+  $a %= 15;
+  echo $a;
+  
+?>
